c16854c210be8d243e35f1fb2d4e6ea69c3fdc53
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2022-12-26 04:20:26 +0200

    edit in resposive css text in cursoal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,12 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}" />
+    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
+
 </head>
 <body>
     <div id="app">

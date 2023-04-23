9b0dd9c81bba1191bbd1a9b36cb4a65257be2458
AmrSaleh2234 <amrsaleh@gmail.com>
2023-04-23 17:39:03 +0200

    add vue ,router vue , vuex and tailwind css

---
+++ vue/src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore} from "vuex";
+
+
+const store=createStore({
+    state:{
+        user:{
+            data:{name:"amr"},
+            token:null
+        }
+    },
+    getters:{},
+    actions:{},
+    mutations:{},
+    modules:{},
+
+})
+
+
+export default store;
+
---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+<div>hello</div>
+</template>
+
+<script>
+export default {
+    name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
